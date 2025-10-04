961369c8ed019870b4fd06b1511fdfff0d5e5fff
muralivijay <muralidharan9845@gmail.com>
2025-10-04 19:40:24 +0530

    include: symlink android ion header for build chi-cdk camera HAL.
    Signed-off-by: muralivijay <muralidharan9845@gmail.com>

--- drivers/staging/android/ion/ion.h
+++ drivers/staging/android/ion/ion.h
@@ -23,9 +23,9 @@
 #include <linux/miscdevice.h>
 #include <linux/bitops.h>
 #include <linux/vmstat.h>
+#include <linux/uapi/ion.h>
+#include <linux/uapi/msm_ion.h>
 #include "ion_kernel.h"
-#include "../uapi/ion.h"
-#include "../uapi/msm_ion.h"
 
 #define ION_ADSP_HEAP_NAME	"adsp"
 #define ION_SYSTEM_HEAP_NAME	"system"
