2cfdbadc986e1d65cefe59e2eca623292a02d328
halt-spesn <ffoxy2870@gmail.com>
2026-02-02 00:00:31 +0200

    Revert "bpf, sockmap: convert to generic sk_msg interface"
    This reverts commit be3f9ecc8c1788f8ff23358e29a1ddc971e40e53.

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -1720,11 +1720,11 @@ static int bpf_prog_attach(const union bpf_attr *attr)
 		ptype = BPF_PROG_TYPE_CGROUP_DEVICE;
 		break;
 	case BPF_SK_MSG_VERDICT:
-		ret = sock_map_get_from_fd(attr, prog);
+		ptype = BPF_PROG_TYPE_SK_MSG;
 		break;
 	case BPF_SK_SKB_STREAM_PARSER:
 	case BPF_SK_SKB_STREAM_VERDICT:
-		ret = sock_map_get_from_fd(attr, prog);
+		ptype = BPF_PROG_TYPE_SK_SKB;
 		break;
 	case BPF_FLOW_DISSECTOR:
 		ptype = BPF_PROG_TYPE_FLOW_DISSECTOR;
@@ -1754,7 +1754,7 @@ static int bpf_prog_attach(const union bpf_attr *attr)
 	switch (ptype) {
 	case BPF_PROG_TYPE_SK_SKB:
 	case BPF_PROG_TYPE_SK_MSG:
-		ret = sock_map_get_from_fd(attr, prog);
+		ret = sockmap_get_from_fd(attr, ptype, prog);
 		break;
 	case BPF_PROG_TYPE_LIRC_MODE2:
 		ret = lirc_prog_attach(attr, prog);
@@ -1807,10 +1807,10 @@ static int bpf_prog_detach(const union bpf_attr *attr)
 		ptype = BPF_PROG_TYPE_CGROUP_DEVICE;
 		break;
 	case BPF_SK_MSG_VERDICT:
-		return sock_map_get_from_fd(attr, NULL);
+		return sockmap_get_from_fd(attr, BPF_PROG_TYPE_SK_MSG, NULL);
 	case BPF_SK_SKB_STREAM_PARSER:
 	case BPF_SK_SKB_STREAM_VERDICT:
-		return sock_map_get_from_fd(attr, NULL);
+		return sockmap_get_from_fd(attr, BPF_PROG_TYPE_SK_SKB, NULL);
 	case BPF_FLOW_DISSECTOR:
 		return skb_flow_dissector_bpf_prog_detach(attr);
 	case BPF_LIRC_MODE2:
